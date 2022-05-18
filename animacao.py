3d5cd5f053783c6aa6d453a6689517c60c8793bd
Oscar Eduardo Anacona <oscared26@gmail.com>
2022-05-18 13:55:59 -0300

    Update animacao.py
    Mudando o nome das variáveis, incluido Pbest, etc.

--- animacao.py
+++ animacao.py
@@ -1,133 +1,117 @@
 import numpy as np
 import random as rand
 import matplotlib.pyplot as plt
-from random import random,seed
+from random import random, seed
 import math
 
+import matplotlib.cm as cm
+from matplotlib.animation import FuncAnimation, PillowWriter
+
+import matplotlib.animation as animation
+
+
+#Função a ser estudada
 def sphere(x):
-  d=x.shape[0]
-  sum=0
-  for i in range(d):
-    sum=sum+x[i]**2
+    d = x.shape[0]
+    sum = 0
+    for i in range(d):
+        sum = sum + x[i] ** 2
+
+    return sum
+
+# Límites da função Sphere
+x_max = 10
+x_min = -10
+
+#Declara a variável que vai contar o número de iterações
+it = 1
+itmax = 100
+
+#Parâmetros do algoritmo PSO
+c1 = 2.05
+c2 = 2.05
+ini_v = 3
+wmax = 0.9
+wmin = 0.4
+max_v = ini_v / 3
+w = wmax - it * (wmax - wmin) / itmax
+
+
+# número de partículas
+S = 20
+# número de dimensões
+d = 2
+# posição de cada partícula
+x = np.zeros((S, d))
+# velocidade de cada partícula
+v = np.zeros((S, d))
+# posição de melhor localização de cada partícula
+Pbest = np.zeros((S, d))
+# o custo da i-ésima partícula
+fitness = np.zeros((S, 1))
+# O melhor fitness local visitado pela i-ésima partícula
+Pbest_fitness = 1e10 * np.ones([S, 1])
+
+
+# Inicialização de x, v e Pbest
+for i in range(S):
+    for j in range(d):
+        x[i, j] = x_min + (x_max - x_min) * random()
+        v[i, j] = ini_v
+
+Pbest = np.copy(x)
+
+while (it < itmax):
   
-  return sum
+    # Para cada partícula
+    for i in range(S):
+        # Avalie o fitness da função objetivo
+        fitness[i, 0] = sphere(x[i, :])
+        # Encontra o melhor fitness e a posição
+        if fitness[i, 0] < Pbest_fitness[i, 0]:
+            Pbest[i, :] = x[i, :]
+            Pbest_fitness[i, 0] = fitness[i, 0]
 
-def rastrigin(x):
-  d=x.shape[0]
-  sum=0
-  for i in range(d):
-    sum=sum+x[i]**2-10*np.cos(2*np.pi*x[i])
-  
-  return 10*d+sum
+    # Encontra o melhor fitness da população
+    bestfitness = np.amin(Pbest_fitness)
+    result = np.where(Pbest_fitness == np.amin(Pbest_fitness))
+    p = result[0]
 
+    # Posição da melhor partícula
+    Gbest = Pbest[p, :]
 
+    # Para cada partícula
+    for j in range(d):
+        for i in range(S):
+            # Gera valores aleatórios para dar aleatoridade à busca
+            r1 = random()
+            r2 = random()
 
-num_variables=2
-generation=0
-x_max=5.12
-x_min=-5.12
+            # Atualiza a nova velocidade
+            v[i, j] = w * v[i, j] + c1 * r1 * (Pbest[i, j] - x[i, j]) + c2 * r2 * (Gbest[0, j] - x[i, j])
 
-
-plt.ion()
-fig = plt.figure()
-ax = fig.add_subplot(111)
-#ax.grid(True)
-
-
-# create data points
-xx = np.linspace(-5.12, 5.12, 100)
-yy = np.linspace(-5.12, 5.12, 100)
-# create grid
-x1, x2 = np.meshgrid(xx, yy)
-
-l=[x1,x2]
-arr = np.array(l)
-
-z = rastrigin(arr)
-
-#fig, ax = plt.subplots()
-# set labels
-
-
-c1=2.05
-c2=2.05
-ini_v=3
-
-maxiter = 100
-w0 = 0.9
-wf=0.1
-slope = (wf-w0)/maxiter
-w= w0
-max_v = ini_v/3
-
-
-S=20 #número de partículas
-
-d=2
-
-x=np.zeros((S,d))
-v=np.zeros((S,d))
-y=np.zeros((S,d))
-    
-part,dim=x.shape
-    
-    
-f_ind=1e10*np.ones([part,1])
-fx=np.zeros((part,1))
-    
-for i in range(part):
-    for j in range(dim):
-        x[i,j]=x_min+ (x_max-x_min) * random()
-        v[i,j]=ini_v
-        y[i,j]=1e10
-    
-t=0
-    
-while(generation < 100):
-    cp=ax.contourf(x1, x2, z, 20, cmap='viridis') 
-    for i in range(part):
-        fx[i,0]=rastrigin(x[i,:])
-                
-        if fx[i,0]<f_ind[i,0]:
-            y[i,:]=x[i,:]
-            f_ind[i,0]=fx[i,0]    
-    
-    
-    bestfitness=np.amin(f_ind)
-    result = np.where(f_ind == np.amin(f_ind))
-    p=result[0]
-    
-    ys=y[p,:]#posição da melhor partícula
-    
-    for j in range(dim):
-        for i in range(part):             
-            r1=random()
-            r2=random()
-                      
-            v[i,j]=w*v[i,j] + c1*r1*(y[i,j]-x[i,j]) + c2*r2*(ys[0,j]-x[i,j])
-    
-            if math.fabs(v[i,j])>max_v:
-                if v[i,j]>0:
-                    v[i,j] = max_v
+            # Limita a velocidade de cada partícula
+            # Estabelecendo valor máximo e mínimo da velocidade
+            if math.fabs(v[i, j]) > max_v:
+                if v[i, j] > 0:
+                    v[i, j] = max_v
                 else:
-                    v[i,j] = -max_v
-                    
-            x[i,j]=x[i,j] + v[i,j]
-          
-    w=w+slope    
-    
-    generation=generation+1
-    
-    print('Generacion: ' + str(generation) + ' - - - Gbest: ' +str(ys[0,:]))
-    
-    line1 = ax.plot(x[:,0], x[:,1], 'ro', linewidth=2, markersize=4)
-    line2 = ax.plot(ys[0,0],ys[0,1], 'o',color='orange', linewidth=2, markersize=4)    
-    
-    
-    ax.set_xlim(x_min, x_max)
-    ax.set_ylim(x_min, x_max)
-   
-    #fig.canvas.draw()
-    plt.pause(0.1)
-    ax.clear()
-    ax.grid(True)
+                    v[i, j] = -max_v
+
+            # Atualiza a nova posição
+            x[i, j] = x[i, j] + v[i, j]
+
+            # Limita a posição de cada partícula
+            # Estabelecendo valor máximo e mínimo da posição
+            if x[i, j] > x_max:
+                x[i, j] = x_max
+
+            if x[i, j] < x_min:
+                x[i, j] = x_min
+
+    # Calcula o fator de inercia
+    w = wmax - it * (wmax - wmin) / itmax
+    # Incremento da geração
+    it = it + 1
+
+    print('Generacion: ' + str(it) + ' - - - Gbest: ' + str(Gbest[0, :]) + ' F(x)= ' + str(bestfitness))
